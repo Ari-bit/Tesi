020b0b8ec0b440292c6e09a713ca5d29c277e001
Arianna <ariannadalessandro9@gmail.com>
2020-10-23 13:25:35 +0200

    door animation

---
+++ prova oculus/Assets/Scripts/EnterTrain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTrain : MonoBehaviour
+{
+     Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Enter()
+    {
+        Debug.Log("arrivato");
+        _animator.SetTrigger("OpenDoor");
+    }
+}
