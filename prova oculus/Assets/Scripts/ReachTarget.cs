c5509ea11c37274af3d2ae0d8b4b9395780d430d
Arianna <ariannadalessandro9@gmail.com>
2020-11-05 19:08:08 +0100

    reach target and sit

--- prova oculus/Assets/Scripts/ReachTarget.cs
+++ prova oculus/Assets/Scripts/ReachTarget.cs
@@ -1,19 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class ReachTarget : MonoBehaviour
 {
-    [SerializeField] private GameObject target;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject _targetFeedback;
+
+    private NavMeshAgent _navMeshAgent;
+    Animator _animator;
+
     void Start()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        if (_targetFeedback != null)
+            _targetFeedback.SetActive(false);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //if (Input.GetMouseButtonDown(0))
+        if (_animator.GetBool("OpenDoor")==true)
+        {
+            _navMeshAgent.SetDestination(_targetFeedback.transform.position);
+        }
+
+        if (_targetFeedback != null)
+            _targetFeedback.SetActive(!TargetReached());
     }
+
+    private bool TargetReached()
+    {
+        if (!_navMeshAgent.pathPending)
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+                if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f)
+                    return true;
+
+        return false;
+    }
+
 }
