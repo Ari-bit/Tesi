c2327a0aa7b0e4e954451678bc89f0f18edc528b
Arianna <ariannadalessandro9@gmail.com>
2020-11-20 18:32:41 +0100

    init

---
+++ VR for anxiety/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject avatarPrefab;
+    [SerializeField] public RuntimeAnimatorController controller;
+    private int spawnIndex;
+    private Transform[] spawnpoints;
+    private Vector3 spawnPos;
+    private int count;
+    [SerializeField] private AvatarManager initAvatar;
+
+    void Start()
+    {
+        count = transform.childCount;
+        spawnpoints = new Transform[count];
+        for (int i = 0; i < count; i++)
+        {
+            spawnpoints[i] = transform.GetChild(i);
+        }
+
+        InvokeRepeating("spawnAvatars", 1, 5);
+    }
+
+    void spawnAvatars()
+    {
+        spawnIndex = Random.Range(0, count);
+
+        GameObject avatar= Instantiate(avatarPrefab, spawnpoints[spawnIndex].position, avatarPrefab.transform.rotation, transform.parent);
+        //initAvatar = new InitializeAvatar();
+        initAvatar.Init(avatar, controller);
+    }
+}
