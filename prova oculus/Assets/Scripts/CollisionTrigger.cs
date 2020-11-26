036017ae8ce270c2133b4d0d7049c767a499b5ba
Arianna <ariannadalessandro9@gmail.com>
2020-11-26 18:32:37 +0100

    merge

---
+++ prova oculus/Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider avatar)
+    {
+        Debug.Log("react");
+        _animator.SetTrigger("Bump");
+    }
+}
---
+++ prova oculus/Assets/Scripts/ReachTarget.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ReachTarget : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+
+    private NavMeshAgent _navMeshAgent;
+    Animator _animator;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        if (_target != null)
+            _target.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Walking"))
+        {
+            _navMeshAgent.SetDestination(_target.transform.position);
+            TargetReached();
+        }
+
+        //if (_target != null)
+        //    _target.SetActive(!TargetReached());
+    }
+
+    private bool TargetReached()
+    {
+        if (!_navMeshAgent.pathPending)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                //if (!_navMeshAgent.hasPath)
+                //{
+                    _animator.SetTrigger("Sit");
+                    float newRotation = _target.transform.rotation.y;
+                    transform.Rotate(0f,newRotation,0f);
+                    return true;
+                //}
+            }
+        }
+
+        return false;
+    }
+
+}
