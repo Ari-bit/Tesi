91b0652184284f2a34223abb6ae60f19c846fa40
Arianna <ariannadalessandro9@gmail.com>
2021-04-19 19:05:08 +0200

    start of flow manager

--- VR for anxiety/Assets/Scripts/Flow/FlowManager.cs
+++ VR for anxiety/Assets/Scripts/Flow/FlowManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class FlowManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private float waitTime = 60.0f;
+    private float timer = 0.0f;
+    [SerializeField] private int many = 50;
+    [SerializeField] private int few = 10;
+    [SerializeField] private SlidersUI slider;
+
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+       if (timer > waitTime)
+        {
+            //evento
+            timer = timer - waitTime;   //azzero
+            if(slider.GetAvatarCount()==few)
+                slider.SetAvatarCount(many);
+            else 
+                slider.SetAvatarCount(few);
+        }
     }
+    
 }
+
--- VR for anxiety/Assets/Scripts/SlidersUI.cs
+++ VR for anxiety/Assets/Scripts/SlidersUI.cs
@@ -7,8 +7,9 @@ public class SlidersUI : MonoBehaviour
 {
 
     private float moodValue = 0.0f;
-    private float avatarCount = 50;
+    private float avatarCount = 10;
     private float controllerRadius = 0.5f;
+    private float visualTime = 0.0f;
 
     void OnGUI()
     {
@@ -25,6 +26,11 @@ public class SlidersUI : MonoBehaviour
 
         GUI.Box(new Rect(Screen.width-130-10, 10, 130, 50), "Time Controller: " + Time.timeScale);
         Time.timeScale = GUI.HorizontalSlider(new Rect(Screen.width - 100- 25, 40, 100, 30), Time.timeScale, 1f, 8f);
+
+        GUIStyle labelDetails = new GUIStyle(GUI.skin.GetStyle("label"));
+        //labelDetails.fontSize = 4 * (Screen.width / 200);
+        GUI.Label(new Rect(Screen.width - 130 - 10, 60, Screen.width - (2 * Screen.width / 8), Screen.height - (2 * Screen.height / 4)),
+            "Timer: " + visualTime.ToString("f0") + " s", labelDetails);
     }
 
     public int GetAvatarCount()
@@ -35,5 +41,16 @@ public class SlidersUI : MonoBehaviour
     {
         return controllerRadius;
     }
+
+    public void SetAvatarCount(int n)
+    {
+        avatarCount = n;
+    }
+
+    void Update()
+    {
+        visualTime += Time.deltaTime; ;
+    }
+
 
 }
